e462dea4b8fcc8cad5efbeb7613f98b2fa53c20d
nandkishor-2103 <mandalnandkishorbk@gmail.com>
2026-08-04 16:28:16 +0530

    implemented coderabbit suggested fixes

--- src/ws-socket/ws-server.socket.js
+++ src/ws-socket/ws-server.socket.js
@@ -26,10 +26,30 @@ export function attachWebSocketServer(server) {
   });
 
   wss.on("connection", (socket) => {
+    socket.isAlive = true;
+    socket.on("pong", () => {
+      socket.isAlive = true;
+    });
+
     sendJson(socket, { type: "welcome" });
 
     socket.on("error", console.error);
   });
+
+  const interval = setInterval(() => {
+    wss.clients.forEach((ws) => {
+      if (ws.isAlive === false) {
+        ws.terminate();
+      }
+      ws.isAlive = false;
+      ws.ping();
+    })
+  }, 3000);
+
+  wss.on("close", () => {
+    clearInterval(interval);
+  });
+
 
   function broadcastMatchCreated(match) {
     broadcast(wss, {
